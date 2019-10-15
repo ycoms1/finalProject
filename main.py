24ee88a1c9475231e1fe429f861c745a57fff41f
ycoms1 <ycoms1@gmail.com>
2019-10-15 16:35:31 +0300

    update main Package for Ubuntu

--- main.py
+++ main.py
@@ -13,7 +13,50 @@ def Install_centos_packages():
 
 
 def Install_ubuntu_packages():
-    print()
+# Python
+    print("Installing Python....")
+    os.system('sudo apt update')
+    os.system('sudo apt install software-properties-common -y')
+    os.system('sudo add-apt-repository ppa:deadsnakes/ppa')
+    os.system('sudo apt install python3.7 -y')
+    os.system('python3.7 --version')
+
+# Docker
+    print("Installing Docker....")
+    os.system('sudo apt update')
+    os.system('sudo apt install apt-transport-https ca-certificates curl software-properties-common')
+    os.system('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -')
+    os.system('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"')
+    os.system('sudo apt update')
+    os.system('apt-cache policy docker-ce')
+    os.system('sudo apt install docker-ce')
+    os.system('sudo systemctl status docker')
+
+# Ansible
+    print("Installing Ansible...")
+    os.system('sudo apt update')
+    os.system('sudo apt install software-properties-common')
+    os.system('sudo apt-add-repository ppa:ansible/ansible')
+    os.system('sudo apt update')
+    os.system('sudo apt install ansible')
+
+# Net-tools
+    print("Installing Net-Tools....")
+    os.system('sudo apt-get install net-tools')
+# etc/hosts
+    print("Update hosts file for Server...")
+    os.system('sudo -- sh -c "echo 192.168.2.1 controller >> /etc/hosts"')
+    os.system('sudo -- sh -c "echo 192.168.2.2 jenkins-master >> /etc/hosts"')
+
+# Change root password
+    print("changing User Root Password...")
+    os.system('sudo passwd root')
+# Snmp V3
+    print("Installing Snmp.... ")
+    os.system('apt update')
+    os.system('apt install snmpd snmp libsnmp-dev')
+
+
 
 
 def Install_jenkins():
